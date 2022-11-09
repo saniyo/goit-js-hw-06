ca8d968c76ac136b989ec2ff5394ab82aff482e6
saniyo <saniyo.ua@gmail.com>
2022-11-09 09:48:50 +0200

    task 01

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+//1
+const allElements = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${allElements.length}`);
+
+for (const element of allElements) {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.querySelectorAll("li").length}`);
+}
+
+//2
+
+// allElements.forEach(element => {
+//     console.log(`Category: ${element.firstElementChild.textContent}`);
+//     console.log(`Elements: ${element.querySelectorAll("li").length}`);
+// })
