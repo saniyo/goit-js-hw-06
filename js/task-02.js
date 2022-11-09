2ab72dcc768be8096aa053265830eb53ef1b3395
saniyo <saniyo.ua@gmail.com>
2022-11-09 11:12:03 +0200

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+var list = document.getElementById("ingredients");
+var clone = list.cloneNode(true);
+
+ingredients.forEach(ingredient => {
+  var entry = document.createElement("li");
+  entry.textContent = ingredient;
+  entry.classList.add("item");
+  clone.appendChild(entry);
+})
+
+list.parentNode.replaceChild(clone, list);
+
+
