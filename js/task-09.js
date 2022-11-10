b4583e6fd846c0bdb411264e60979997f66b817e
saniyo <saniyo.ua@gmail.com>
2022-11-10 11:06:51 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body"),
+      btnChangeColor = document.querySelector(".change-color"),
+      text = document.querySelector(".color");
+
+const changeColorClick = (event) => {
+      let randomColor = getRandomHexColor();
+      body.style.backgroundColor = randomColor;
+      text.textContent = randomColor;
+}
+
+btnChangeColor.addEventListener("click", changeColorClick);
+
