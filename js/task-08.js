45fccbfff4780071cfae1156a77dc13148ff2db4
saniyo <saniyo.ua@gmail.com>
2022-11-10 10:50:10 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const   formElements = event.currentTarget.elements,
+            email = formElements.email,
+            password = formElements.password;
+
+    email.value === "" || password.value === ""
+        ? alert("All fields have to be filled")
+        : null;
+    
+    const formData = {
+        email: email.value,
+        password: password.value,
+    };
+    
+    console.log(formData);
+    event.currentTarget.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
